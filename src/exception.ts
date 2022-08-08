c47ca93403a8d84e1855146a3207362e8ead0b14
corentinleberre <corentin.leb29@gmail.com>
2022-08-08 17:50:22 +0200

    :loud_sound: Amélioration des logs d'infos et d'erreurs

--- src/exception.ts
+++ src/exception.ts
@@ -3,38 +3,57 @@
 import { exec } from "child_process";
 import * as fs from "fs";
 
+interface ScriptConfiguration {
+  exceptions: Array<string>;
+}
+
 const [SHA1, SHA2] = process.argv.slice(2, 4);
 
-console.log("new commit: ", SHA1, "compare to: ", SHA2);
+if (SHA1) {
+  if (SHA2) console.log(`INFO : Comparing : ${SHA1} to : ${SHA2}\n`);
+  else console.log(`INFO : Comparing local modifications to : ${SHA1}\n`);
+} else {
+  console.log("INFO : Comparing local modifications to HEAD\n");
+}
 
-const getExceptionsConf = (): { exceptions: Array<string> } =>
-  JSON.parse(fs.readFileSync("./exceptions.conf.json").toString());
+const getExceptionsConf = (): ScriptConfiguration => {
+  try {
+    let fileContents = fs.readFileSync("./exceptions.conf.json");
+    return JSON.parse(fileContents.toString());
+  } catch (err) {
+    console.log(
+      "ERROR : You have to create the file exceptions.conf.json with your exceptions\n"
+    );
+  }
+};
 
-exec(
-  `git diff --name-only ${SHA1 ?? ""} ${SHA2 ?? ""}`,
-  (error, stdout, stderr) => {
-    if (error || stderr)
-      console.log(`error: ${error?.message}\nstderr: ${stderr}`);
-    if (stdout) {
-      const files = stdout.toString().split("\n");
-      const modifiedFilesInNewestCommit = files
-        .slice(0, files.length - 1)
-        .map((file) => file.split("/"));
-      if (modifiedFilesInNewestCommit.length > 0) {
-        const exceptions = getExceptionsConf().exceptions;
-        console.log(
-          "Do not run next command if only theses files were modified :\n",
-          exceptions
-        );
+const exceptionsConf = getExceptionsConf();
 
-        const onlyExceptions = modifiedFilesInNewestCommit.every((path) =>
-          exceptions.some((exception) => exception === path[0])
-        );
-        const result = { modifiedFilesInNewestCommit, onlyExceptions };
-        console.log(result);
-        process.exit(result.onlyExceptions ? -1 : 0);
+if (exceptionsConf?.exceptions?.length > 0) {
+  exec(
+    `git diff --name-only ${SHA1 ?? ""} ${SHA2 ?? ""}`,
+    (error, stdout, stderr) => {
+      if (error || stderr)
+        console.log(`error: ${error?.message}\nstderr: ${stderr}`);
+      if (stdout) {
+        const files = stdout.toString().split("\n");
+        const modifiedFilesInNewestCommit = files
+          .slice(0, files.length - 1)
+          .map((file) => file.split("/"));
+        if (modifiedFilesInNewestCommit.length > 0) {
+          const exceptions = exceptionsConf.exceptions;
+          console.log("Exceptions configured :\n", exceptions, "\n");
+
+          const onlyExceptions = modifiedFilesInNewestCommit.every((path) =>
+            exceptions.some((exception) => exception === path[0])
+          );
+          const result = { modifiedFilesInNewestCommit, onlyExceptions };
+          console.log(result, "\n");
+          process.exitCode = result.onlyExceptions ? -1 : 0;
+        }
       }
     }
-    return;
-  }
-);
+  );
+} else {
+  console.log("ERROR : You have to add exceptions in exceptions.conf.json\n");
+}

54bcc0ab2ab289f513f89835fe92262fc00379f1
corentinleberre <corentin.leb29@gmail.com>
2022-08-19 16:46:56 +0200

    :heavy_plus_sign: Utilisation de @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,20 @@
+import typescript from "@rollup/plugin-typescript";
 import replace from "rollup-plugin-replace";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "dist/exception.js",
+  input: "src/main.ts",
+  output: {
+    file: "dist/exception.js",
+    banner: "#!/usr/bin/env node",
+    format: "cjs",
+  },
   plugins: [
+    typescript(),
     terser(),
     replace({
       delimiters: ["", ""],
       "#!/usr/bin/env node": "",
     }),
   ],
-  output: {
-    file: "dist/exception.js",
-    banner: "#!/usr/bin/env node",
-    format: "cjs",
-  },
 };

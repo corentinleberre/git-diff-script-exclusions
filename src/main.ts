02b78e803e330656abb29441acaba6b4d7f255eb
corentinleberre <corentin.leb29@gmail.com>
2022-09-01 18:37:46 +0200

    :wrench: Rendre target optionnel

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ program
   .name("git-diff-script-exclusions")
   .version("3.1.0", "-v, --vers", "output the current version")
   .description(
-    "Compare two commits and determinate if modified files are strictly limited to path configured\n\n  > See README.md for configuration details"
+    "Compare commits and determinate if modified files are strictly limited to path configured\n\n  > See README.md for configuration details"
   )
   .requiredOption(
     "-s, --source <string>",
     "source commit <commit_sha || branch_name>"
   )
-  .requiredOption(
+  .option(
     "-t, --target <string>",
-    "target commit <commit_sha || branch_name>"
+    "target commit <commit_sha || branch_name>", ''
   );
 
 program.parse(process.argv);
